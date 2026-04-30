fc6328402206538e66b4f65ea60a96b52cf61459
ojauslin <osjauslin@gmail.com>
2026-04-30 14:18:16 +0200

    added routing to html pages and changed file structure to accommodate

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,19 @@ app.config.from_object('config')
 
 @app.route('/')
 def index():
-  return 'Hello World!'
+  return render_template('index.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/lessons')
+def lessons():
+  return render_template('lessons.html')
+
+@app.route('/about-me')
+def about_me():
+  return redirect(url_for('about'))
 
 if __name__ == '__main__':
   app.run()
