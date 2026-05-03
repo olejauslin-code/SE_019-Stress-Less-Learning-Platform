497176693975b0a1d3f3eabd9353d637524109c7
ojauslin <osjauslin@gmail.com>
2026-05-03 12:07:23 +0200

    gunicorn for production start

--- app.py
+++ app.py
@@ -378,4 +378,4 @@ def about_me():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=False)
